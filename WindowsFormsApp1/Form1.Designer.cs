2b3e95c0fae4aad7a99a79350863b66f45b2b507
Luana Faiotto <luana.faiotto@gmail.com>
2020-03-09 20:55:30 -0300

    First Commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmExemplo1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdRun = new System.Windows.Forms.Button();
+            this.nome = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cmdRun
+            // 
+            this.cmdRun.Location = new System.Drawing.Point(317, 97);
+            this.cmdRun.Name = "cmdRun";
+            this.cmdRun.Size = new System.Drawing.Size(102, 20);
+            this.cmdRun.TabIndex = 0;
+            this.cmdRun.Text = "&Executar";
+            this.cmdRun.UseVisualStyleBackColor = true;
+            this.cmdRun.Click += new System.EventHandler(this.cmdRun_Click);
+            this.cmdRun.MouseMove += new System.Windows.Forms.MouseEventHandler(this.cmdRun_MouseMove);
+            // 
+            // nome
+            // 
+            this.nome.AutoSize = true;
+            this.nome.Location = new System.Drawing.Point(35, 101);
+            this.nome.Name = "nome";
+            this.nome.Size = new System.Drawing.Size(35, 13);
+            this.nome.TabIndex = 1;
+            this.nome.Text = "Nome";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(122, 97);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(148, 20);
+            this.txtNome.TabIndex = 2;
+            // 
+            // frmExemplo1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(584, 311);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.nome);
+            this.Controls.Add(this.cmdRun);
+            this.Name = "frmExemplo1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmExemplo1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdRun;
+        private System.Windows.Forms.Label nome;
+        private System.Windows.Forms.TextBox txtNome;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmExemplo1 : Form
+    {
+        public frmExemplo1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdRun_Click(object sender, EventArgs e)
+        {
+            if (txtNome.Text == "")
+            {
+                //Primeiro texto preenche o quadro
+                //Segundo texto preenche a label do quadro
+                //O último é a ação do botão
+                MessageBox.Show("Preencha o campo.", "Opss!", MessageBoxButtons.OK);
+                txtNome.Focus();
+                return;
+            }
+            MessageBox.Show("Olá " + txtNome.Text);
+        }
+
+        private void cmdRun_MouseMove(object sender, MouseEventArgs e)
+        {
+            Random r = new Random();
+
+            //.Left manipula o eixo X
+            txtNome.Left = r.Next(this.Size.Width);
+
+            //.Top manipula o eixo Y
+            txtNome.Top = r.Next(this.Size.Height);
+        }
+
+        private void frmExemplo1_Load(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            this.BackColor = Color.FromArgb(r.Next(255), r.Next(255), r.Next(255));
+        }
+    }
+}
